4a6549b912427494a0e38846ecc30bef2e55c37d
lestrrat <lestrrat+github@gmail.com>
2010-02-10 13:55:02 +0900

    remove unused methods

--- lib/Cache/Memcached/AnyEvent/Protocol.pm
+++ lib/Cache/Memcached/AnyEvent/Protocol.pm
@@ -4,24 +4,8 @@ use strict;
 sub new {
     my $class = shift;
     my $self  = bless {@_}, $class;
-
-    foreach my $cb qw(add_cb decr_cb delete_cb incr_cb get_cb get_multi_cb replace_cb set_cb stats_cb version_cb) {
-        my $method = "_build_$cb";
-        $self->{$cb} ||= $self->$method;
-    }
     return $self;
 }
-
-sub _build_add_cb {}
-sub _build_decr_cb {}
-sub _build_delete_cb {}
-sub _build_incr_cb {}
-sub _build_get_cb {}
-sub _build_get_multi_cb {}
-sub _build_replace_cb {}
-sub _build_set_cb {}
-sub _build_stats_cb {}
-sub _build_version_cb {}
 
 sub prepare_handle {}
 
