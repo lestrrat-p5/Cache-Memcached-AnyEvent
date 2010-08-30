b8972523334996755ed1d562a4d518520de7edc6
lestrrat <lestrrat+github@gmail.com>
2010-08-30 12:31:30 +0900

    fix the tests so that it does what mdorman actually wanted to do

--- t/CMAETest/Dorman.pm
+++ t/CMAETest/Dorman.pm
@@ -4,8 +4,9 @@ use t::Cache::Memcached::AnyEvent::Test;
 
 sub run {
     my ($pkg, $protocol, $selector) = @_;
-    my $cv = AE::cv;
+
     { # populate
+        my $cv = AE::cv;
         my $mc = test_client(
             protocol_class => $protocol,
             selector_class => $selector,
@@ -16,13 +17,11 @@ sub run {
             is $rc, 1, 'Success setting key';
             $cv->end;
         });
+        $cv->recv;
     }
 
-    $cv->recv;
-
-    $cv = AE::cv;
-
-    for (1..40) {
+    { # run once with regular server list
+        my $cv = AE::cv;
         my $mc = test_client(
             protocol_class => $protocol,
             selector_class => $selector,
@@ -33,10 +32,27 @@ sub run {
             is $value, 'bar', 'Success getting key';
             $cv->end;
         });
+        $cv->recv;
     }
 
-    $cv->recv;
-
+    { # run another time, but this time shuffle round the server
+      # list before creating a client
+        my @servers = reverse split /\s*,\s*/, $ENV{PERL_ANYEVENT_MEMCACHED_SERVERS};
+        
+        local $ENV{PERL_ANYEVENT_MEMCACHED_SERVERS} = join(',', @servers);
+        my $cv = AE::cv;
+        my $mc = test_client(
+            protocol_class => $protocol,
+            selector_class => $selector,
+            namespace => 'mytest.') or die;
+        $cv->begin;
+        $mc->get (foo => sub {
+            my $value = shift;
+            is $value, 'bar', 'Success getting key';
+            $cv->end;
+        });
+        $cv->recv;
+    }
     done_testing;
 }
 
